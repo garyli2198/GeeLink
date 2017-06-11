9d68d45ca53573de7e1a8ecc79f6144793c35e30
Gary Li <gary.li@berkeley.edu>
2017-06-11 10:17:28 -0700

    ability to delete groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611100944) do
+ActiveRecord::Schema.define(version: 20170611170514) do
 
   create_table "follows", force: :cascade do |t|
     t.string "followable_type"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170611100944) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "author"
+    t.integer "author_id"
   end
 
   create_table "users", force: :cascade do |t|
